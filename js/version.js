5b4eb19045e219f886ce2878ca05ed9f683f0853
tetsuakibaba <tetsuaki.baba@gmail.com>
2024-06-14 18:15:31 +0900

    version情報管理の整理

---
+++ js/version.js
@@ -0,0 +1,6 @@
+var version = 'v2.1.1';
+// versionからfull_versionを生成
+var full_version = version;
+// version からマイナー番号までのショートバージョンを生成
+var short_version = version.split('.').slice(0, 2).join('.');
+
